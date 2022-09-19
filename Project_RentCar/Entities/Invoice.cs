e1b9d6283dc3f989455bb7709e3ebfccba1b1bbd
Marcio Rodrigo <marciodev0614@gmail.com>
2022-09-19 16:08:14 -0300

    Inserção do projeto RentCar

---
+++ Project_RentCar/Entities/Invoice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Course.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment // Exemplo de propriedade calculada
+        {
+            get { return BasicPayment + Tax;}
+        }
+
+        public override string ToString()
+        {
+            return $"Basic payment: {BasicPayment.ToString("f2", CultureInfo.InvariantCulture)}\nTax: " +
+                $"{Tax.ToString("f2", CultureInfo.InvariantCulture)}\nTotal payment: {TotalPayment.ToString("f2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
