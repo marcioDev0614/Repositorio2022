eea90a06f79ac29991008acf8d795b39cf0e0f1d
Marcio Rodrigo <marciodev0614@gmail.com>
2022-10-05 16:53:31 -0300

    ProgramaMediaAluno

--- ProjectsAndStudies/Course/Course/Program.cs
+++ ProjectsAndStudies/Course/Course/Program.cs
@@ -3,38 +3,15 @@ using System.Globalization;
 
 namespace Course
 {
+
     class Program
     {
-        static void Main_(string[] args)
+        static void Main(string[] args)
         {
-            double xA, xB, xC, yA, yB, yC;
+            Console.WriteLine("**************");
+            Console.WriteLine("****Teste*****");
 
-            Console.WriteLine("Entre com as medidas do triangulo X");
-            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine("Entre com as medidas do triangulo Y");
-            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double p = (xA + xB + xC) / 2;
-            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
-
-            p = (yA + yB + yC) / 2;
-            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
-
-            Console.WriteLine("área de X = " + areaX.ToString("f2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Area de Y = " + areaY.ToString("f2", CultureInfo.InvariantCulture));
-
-            if(areaX > areaY)
-            {
-                Console.WriteLine("Maior área: X");
-            }
-            else
-            {
-                Console.WriteLine("Maior área: Y");
-            }
         }
     }
 }
---
+++ ProjectsAndStudies/Course/Course/ProgramaMediaAluno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    public class NotaAluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+
+        public double Media()
+        {
+            return Nota1 + Nota2 / 2;
+        }
+
+        public override string ToString()
+        {
+            return $"O aluno: {Nome}\nNota 1: {Nota1}\nNota 2: {Nota2}\nMédia: " + Media().ToString("f2", CultureInfo.InvariantCulture);
+        }
+    }
+
+
+    class ProgramMediaAluno
+    {
+        static void Main_(string[] args)
+        {
+            Console.WriteLine("**************");
+            Console.WriteLine("****Teste*****");
+            Console.WriteLine("______________");
+
+            NotaAluno aluno1 = new NotaAluno();
+            NotaAluno aluno2 = new NotaAluno();
+
+            Console.Write("Nome do aluno 1: ");
+            aluno1.Nome = Console.ReadLine();
+            Console.Write("Informe a nota 1 do aluno: ");
+            aluno1.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Informe a nota 2 do aluno : ");
+            aluno1.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine(aluno1);
+
+        }
+    }
+}
