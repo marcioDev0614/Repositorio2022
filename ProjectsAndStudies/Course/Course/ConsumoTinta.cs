27b79a5354a05871f9f28df87bbaa102a9d6bff0
Marcio Rodrigo <marciodev0614@gmail.com>
2022-10-05 21:03:36 -0300

    Programa_Consumo_Tinta

---
+++ ProjectsAndStudies/Course/Course/ConsumoTinta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Course
+{
+    class ConsumoTinta
+    {
+        public class AreaPintura
+        {
+            public double Largura;
+            public double Altura;
+
+            public double QtdTinta()
+            {
+                return (Largura * Altura) / 2;
+            }
+
+            public double Area()
+            {
+                return Largura * Altura;
+            }
+            public override string ToString()
+            {
+                return $"Largura: {Largura}\nAltura: {Altura}\nÁrea: " 
+                    + Area().ToString("f2", CultureInfo.InvariantCulture)+"\nQtd Tinta: "
+                    + QtdTinta().ToString("f2", CultureInfo.InvariantCulture)+" litros";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********************");
+            Console.WriteLine("    Consumo de Tinta   ");
+            Console.WriteLine("***********************");
+
+            AreaPintura area = new AreaPintura();
+
+            Console.Write("Informe a largura da parede: ");
+            area.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Informe a altura da parede: ");
+            area.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(area);
+
+        }
+    }
+}
+
