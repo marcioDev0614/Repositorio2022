26cc71fa2d1ae709e6778e114011016c47539ffe
Marcio Rodrigo <marciodev0614@gmail.com>
2022-10-13 10:29:13 -0300

    Ordem sugerida para

--- ProjectsAndStudies/Course/Course/Executar.cs
+++ ProjectsAndStudies/Course/Course/Executar.cs
@@ -9,14 +9,17 @@ namespace Course
     {
         static void Main_()
         {
-            Console.Write("Qual é a cotação do dólar? ");
-            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Quantos dólares você vai comprar? ");
-            double qtdDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double total = ConversorMoeda.Conversor(cotacao, qtdDolar);
-            Console.WriteLine();
-            Console.WriteLine("O valor a ser pago em reais R$" + total.ToString("f2",CultureInfo.InvariantCulture));
+            // Ordem sugerida para implementação de membros
+
+            //  Ordem sugerida
+
+            //• Atributos privados
+            //• Propriedades autoimplementadas
+            //• Construtores
+            //• Propriedades customizadas
+            //• Outros métodos da classe
+
         }
     }
 }
