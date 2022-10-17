b4421e18e1ef7b18118e0126acf633f49686768f
Marcio Rodrigo <marciodev0614@gmail.com>
2022-10-17 20:50:20 -0300

    Vetores part1 - Programa para mostrar a média de altura

--- ProjectsAndStudies/Course/Course/Executar.cs
+++ ProjectsAndStudies/Course/Course/Executar.cs
@@ -10,57 +10,25 @@ namespace Course
         static void Main()
         {
 
-            // Nullable: É um recurso de C# para que dados de tipo valor(struct) possam
-            // receber o valor null.
-
-            // Uso comum?
-            // Campo de banco de dados que podem valer nulo(data de nascimento, algum valor numérco, etc)
-            // Dados e parametros opcionais.
-
-            // Para identificar o tipo de uma variavel como nullable, adicionamos a sintaxe o 
-            // Simbolo de "?"
-            // EX:
-            double? x = null;
-            double? y = 10.0;
-
-            Console.WriteLine(x.GetValueOrDefault());// O resultado retorna o valor padrão caso não tenha valor setado. No caso é zero
-            Console.WriteLine(y.GetValueOrDefault()); // O resultado será 10
-            Console.WriteLine();
-            Console.WriteLine(x.HasValue); // Aqui ele retornar false, pois não tem valor
-            Console.WriteLine(y.HasValue); // Aqui ele retorna true
-            Console.WriteLine();
-            //Console.WriteLine(x.Value); // Aqui o resultado é uma exception. Nesse caso se faz necessário fazer uma validação antes. Exemplo
-                                        // usando um IF
-            if (x.HasValue)
-            {
-                Console.WriteLine(x.Value);
-            }
-            else
-            {
-                Console.WriteLine("X is null");
-            }
-
-            if (y.HasValue)
-            {
-                Console.WriteLine(y.Value);
-            }
-            else
-            {
-                Console.WriteLine("Y is null");
-            }
-
-            // Operador de Coalecência nula.
-            // Nesse exemplo a variável "b" que recebe o atributo de "a", Retornará o valor de 0.00 caso a seja null.
-            double? a = null;
-            double? b = 10.0;
-
-            double c = a ?? 5;
-            double d = b ?? 5;
-
-            Console.WriteLine(c);
-            Console.WriteLine(d);
-
+            // Programa para ler um número inteiro n e a altura de n pessoas. Armazenar as N alturas
+            // em um vetor. Em seguida, mostrar a altura média dessas pessoas .
             
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n]; // Instanciando um vetor
+            for(int i = 0; i < n; i++) // Laço de repetição
+            {
+                vect[i] = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            }
+
+            double sum = 0.0; // Criando uma variavel para acumular o vetor.
+            for(int i = 0; i < n; i++)
+            {
+                sum += vect[i]; // recebendo o valor do vetor
+            }
+
+            double avg = sum / n;
+            Console.WriteLine("AVEREGTH HEIGHT = " + avg.ToString("F2",CultureInfo.InvariantCulture));
 
         }
     }
