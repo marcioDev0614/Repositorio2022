f2ee7f4a09e74040d29abfdb3d4bf30f08a85c17
Marcio Rodrigo <marciodev0614@gmail.com>
2022-10-20 12:44:02 -0300

    Programa_Aluguel_Quarto

--- ProjectsAndStudies/Course/Course/Executar.cs
+++ ProjectsAndStudies/Course/Course/Executar.cs
@@ -10,37 +10,76 @@ namespace Course
         static void Main()
         {
 
+            Rent[] vect = new Rent[10];
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                vect[quarto] = new Rent { Nome = nome, Email = email };
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+
+
+
             // Programa para ler um número inteiro n e a altura de n pessoas. Armazenar as N alturas
             // em um vetor. Em seguida, mostrar a altura média dessas pessoas .
 
 
-            Console.WriteLine("Descobrir a média final do aluno: ");
-            Console.Write("Informe o nome do aluno: ");
-            string nome = Console.ReadLine();
-            Console.Write($"Informe a quantidade de notas : ");
-            int qtdNotas = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Preço médio do produto ");
+            //Console.Write("Informe a quantidade de produtos: ");
+            //int n = int.Parse(Console.ReadLine());
 
-            double[] notas = new double[qtdNotas];
+            //Product[] vect = new Product[n];
+            //for(int i = 0; i < n; i++)
+            //{
+            //    string name = Console.ReadLine();
+            //    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //    vect[i] = new Product { Name = name, Price = price };
+            //}
 
-            for(int i = 0; i < qtdNotas; i++)
-            {
-                Console.Write($"Nota: {i}: ");
-                notas[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                
-            }
+            //double sum = 0.0;
+            //for(int i = 0; i < n; i++)
+            //{
+            //    sum += vect[i].Price;
+            //}
 
-            double sumNotas = 0.0;
-            for(int i = 0; i < qtdNotas; i++)
-            {
-                sumNotas += notas[i];
-            }
+            //double media = sum / n;
+            //Console.WriteLine("AVERAGE PRICE: " + media.ToString("F2",CultureInfo.InvariantCulture));
 
-            double media = sumNotas / 3;
-            string result = media >= 7.0 ? "Aprovado" : "Reprovado";
-            Console.WriteLine();
-            Console.WriteLine($"Aluno: {nome}\nMédia: " + media.ToString("f2",CultureInfo.InvariantCulture) 
-                                +"\nResultado Final: " + result);
-            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             //Console.WriteLine("Programa para ler a média de altura");
             //int n = int.Parse(Console.ReadLine());
             //double[] vect = new double[n];
