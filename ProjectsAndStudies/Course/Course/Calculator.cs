20ad16725d7d7616d66cc4ecca77e4321ec85b42
Marcio Rodrigo <marciodev0614@gmail.com>
2022-10-21 11:00:48 -0300

    Modificadores de parametros ref e out

--- ProjectsAndStudies/Course/Course/Calculator.cs
+++ ProjectsAndStudies/Course/Course/Calculator.cs
@@ -33,7 +33,30 @@ namespace Course
             return sum;
         }
 
+        public static double MultiNotas(params double[] notas)
+        {
+            double result = 0.0;
+            for(int i = 0; i < notas.Length; i++)
+            {
+                result += notas[i];
+            }
+            return result;
+        }
 
+        //public static void Triple(int x) // Metoto ruim
+        //{
+        //    x = x * 3;
+        //}
+
+        //public static void Triple(ref int x) // Metoto usando ref no parametro.
+        //{                                    // Atenção! usando o ref, quando chamamos a função, a variável deve ser iniciada.
+        //    x = x * 3;
+        //}
+
+        public static void Triple(int origin, out int result) // Metodo usando out no parametro
+        {        // Diferente do ref, o sintaxe out, não obriga que a variável original seja iniciada                               
+            result = origin * 3;
+        }
 
     }
 }
--- ProjectsAndStudies/Course/Course/Executar.cs
+++ ProjectsAndStudies/Course/Course/Executar.cs
@@ -9,75 +9,10 @@ namespace Course
     {
         static void Main()
         {
-
-            int s1 = Calculator.SumInt(2, 3);
-            int s2 = Calculator.SumInt(4, 5, 8);
-            Console.WriteLine("Metodo não recomendado: " + s1);
-            Console.WriteLine("Metodo não recomendado: " + s2);
-
-            int s3 = Calculator.SumIntVetor(new int[] { 10, 10, 2 });
-            Console.WriteLine("Metodo não tão recomendado " + s3);
-            int s4 = Calculator.SumIntVetorParans(15,78,78,12);
-            Console.WriteLine("Resultado via metodo Params: " + s4);
-
-
-            // Programa para ler um número inteiro n e a altura de n pessoas. Armazenar as N alturas
-            // em um vetor. Em seguida, mostrar a altura média dessas pessoas .
-
-
-            //Console.WriteLine("Preço médio do produto ");
-            //Console.Write("Informe a quantidade de produtos: ");
-            //int n = int.Parse(Console.ReadLine());
-
-            //Product[] vect = new Product[n];
-            //for(int i = 0; i < n; i++)
-            //{
-            //    string name = Console.ReadLine();
-            //    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //    vect[i] = new Product { Name = name, Price = price };
-            //}
-
-            //double sum = 0.0;
-            //for(int i = 0; i < n; i++)
-            //{
-            //    sum += vect[i].Price;
-            //}
-
-            //double media = sum / n;
-            //Console.WriteLine("AVERAGE PRICE: " + media.ToString("F2",CultureInfo.InvariantCulture));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            //Console.WriteLine("Programa para ler a média de altura");
-            //int n = int.Parse(Console.ReadLine());
-            //double[] vect = new double[n];
-
-            //for(int i = 0; i < n; i++)
-            //{
-            //    vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //}
-
-            //double sum = 0.0;
-            //for(int i = 0; i < n; i++)
-            //{
-            //    sum += vect[i];
-            //}
-
-            //double avg = sum / n;
-            //Console.WriteLine("Média: " + avg.ToString("f2",CultureInfo.InvariantCulture));
+            int a = 10;
+            //Calculator.Triple(ref a); // Camando o metodo sem o out ou ref, o valor não será multiplicado
+            Calculator.Triple(ref a); // Adicionando o "ref", o valor declararo será multiplicado.
+            Console.WriteLine(a);
 
         }
     }
