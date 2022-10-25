85adbec2fe5b1c1f6ed3a14242db2da7ede6f24e
Marcio Rodrigo <marciodev0614@gmail.com>
2022-10-25 20:06:57 -0300

    Praticas usando List<>

--- ProjectsAndStudies/Course/Course/Executar.cs
+++ ProjectsAndStudies/Course/Course/Executar.cs
@@ -2,30 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Globalization;
-
 namespace Course
 {
     class Executar
     {
-        static void Main()
+        static void Main_()
         {
-            // Laço foreach - sintaxe opcional e simplificada para percorrer coleções
-
-            string[] vect = new string[] { "Marcio", "Jouze" };
-            foreach(var nomes in vect) // 
-            {
-                Console.WriteLine(nomes);
-            }
-
-            Console.WriteLine("--------------------------");
-
-            for(int i = 0; i < vect.Length; i++) // Mesmo resultado do foreach usand o for
-            {
-                Console.WriteLine(vect[i]);
-            }
-
-
+          
 
         }
+
+
     }
 }
---
+++ ProjectsAndStudies/Course/Course/Listas.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+
+namespace Course
+{
+    class Listas
+    {
+        static void Main()
+        {
+            // Lista é uma estrutura de dados
+            // Homogênea (dados do mesmo tipo)
+            // Ordenada(Elementos acessados por meio de posições)
+            // Inicia vazia, e seus elementos são alocados sob demanda
+
+            // Classe: List
+            // Namespace: System.Collection.Generic
+
+            // Sintaxe de list já instanciando os elementos.
+
+            List<string> list = new List<string> { "Maria", "Alex", "Bob", "Anna" };
+            list.Insert(2, "Marco"); // Inserindo a string numa determinada posição.
+
+
+            foreach (var obj in list) // Percorrer toda a lista com o foreach
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine("List count: " + list.Count);
+
+            string s1 = list.Find(x => x[0] == 'A'); // Exemplo de metodo usando expressão lambda
+            Console.WriteLine("Firts A: " + s1);
+
+            string s2 = list.FindLast(x => x[0] == 'A'); // Ultimo elemento da lista
+            Console.WriteLine("Find last A: " + s2);
+
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("Fisrt position 'A': " + pos1); // Posição do elemento na lista
+
+            int pos2 = list.FindLastIndex(x => x[0] == 'A'); // Ultima posição index da lista
+            Console.WriteLine("Last index position: 'A': " + pos2);
+
+            // Sintaxe para encontrar todos os elementos de tamanho 5
+            List<string> lis2 = list.FindAll(x => x.Length == 5);
+            Console.WriteLine("------------------------------");
+            foreach(var obj in lis2)
+            {
+                Console.WriteLine(obj);
+            }
+
+            list.Remove("Alex"); // Removendo um elemento da lista
+            Console.WriteLine("-------------------------");
+            foreach (var obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            list.RemoveAll(x => x[0] == 'M');// Removendo todos os elementos que começam com a letra "M"
+            Console.WriteLine("-------------------------");
+            foreach (var obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+
+        }
+    }
+}
